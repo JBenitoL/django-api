7ad4ecfbd02ecc7a4e645f100b0273f859f513e0
jbenitoL <jbenitollorens@gmail.com>
2022-06-30 18:44:12 +0200

    debug jenkins

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+echo "START"
 export WORKSPACE=`pwd`
+echo "START2"
+
 # Create/Activate virtualenv
 virtualenv venv
+echo "START3"
 source venv/bin/activate
+echo "START4"
 # Install Requirements
 pip install -r requirements.txt
+echo "START5"
 # Run tests
 python manage.py makemigrations
+echo "START6"
 python manage.py migrate
+echo "START7"
 python manage.py test
+echo "START8"
